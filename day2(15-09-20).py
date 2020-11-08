011c682bcd1b6ace416f8a72009ded017b2bfc80
sowjanyakare <74136988+sowjanyakare@users.noreply.github.com>
2020-11-08 17:03:04 +0530

    first commit

---
+++ day2(15-09-20).py
@@ -0,0 +1,20 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> 5+8
+13
+>>> 
+== RESTART: C:/Users/HP/Desktop/project/python practice/day2(15-09-2020).py ==
+hello everyone
+welcome to python programming
+>>> 
+== RESTART: C:/Users/HP/Desktop/project/python practice/day2(15-09-2020).py ==
+hello everyone
+welcome to python programming
+sum= 49
+>>>2u='hi i am sowjanya'
+
+
+
+
